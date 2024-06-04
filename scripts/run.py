c24291d47a4b9fe948d0777f190bf8c98934ad11
Samuel Hoyos <samuel.hoyos@udea.edu.co>
2024-06-04 13:27:46 +0200

    Creating script

---
+++ scripts/run.py
@@ -0,0 +1,8 @@
+from e_fish import load 
+from pathlib import Path
+
+if __name__ == "__main__":
+    
+    df_1=load.get_df(channel="C1", folder=Path("2024_05_15/position_1"))
+    df_2=load.get_df(channel="C2", folder=Path("2024_05_15/position_1"))
+    df_3=load.get_df(channel="C3", folder=Path("2024_05_15/position_1"))
