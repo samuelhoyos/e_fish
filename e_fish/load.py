b757679dd053dcc839aa28f4ee32342ce55f1f1a
Samuel Hoyos <samuel.hoyos@udea.edu.co>
2024-06-11 10:51:57 +0200

    Load cache and optimization

--- e_fish/load.py
+++ e_fish/load.py
@@ -6,18 +6,23 @@ import os
 import numpy as np
 from tqdm import tqdm
 from diskcache import Cache
+import math
 
-cache_dir = Path(
-    __file__
-).parent  #'/home/samuel/Documents/Internship/STAGE/Data_analysis/data/'
+
+cache_dir = (
+    Path(__file__).parent / "cache_load"
+)  #'/home/samuel/Documents/Internship/STAGE/Data_analysis/data/'
 os.makedirs(cache_dir, exist_ok=True)
-cache = Cache(cache_dir)
+cache = Cache(cache_dir, size_limit=math.inf)
 
 
 def reader(filename):
     df = pd.read_csv(filename, skiprows=4, delimiter=";")
     df.index = [int(filename.split("--")[-1].split(".")[0])] * len(df)
     return df
+
+
+cache.memoize()
 
 
 def create_dfs(file_list: tuple):
@@ -41,9 +46,25 @@ def get_df(channel: str, folder: str):
     file_list = tuple(
         [str(folder / i) for i in os.listdir(folder) if i.split("-")[0] == channel]
     )
-    dfs = create_dfs(file_list[0:1000])
+
+    dfs = create_dfs(file_list)
     df = pd.concat(dfs)
+    df.index.name = "file_number"
+    df.set_index("Time", append=True, inplace=True)
+    df.sort_index(inplace=True)
     df.reset_index(inplace=True)
     df.columns = ["file_number", "time", "amplitude"]
-    df.sort_values(["file_number", "time"], inplace=True)
+    return df
+
+
+def avg_amplitude(df: pd.DataFrame, window_size: int):
+    df_avg = (
+        df.groupby("file_number")
+        .amplitude.rolling(window=window_size, min_periods=3)
+        .mean()
+        .fillna(method="bfill")
+        .to_frame("avg_amplitude")
+    )
+    df_avg.reset_index(level="file_number", inplace=True)
+    df["avg_amplitude"] = df_avg.avg_amplitude
     return df
