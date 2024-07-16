bd412ada99e932b09498cb8fac8c87468b76f684
Samuel Hoyos <samuel.hoyos@udea.edu.co>
2024-07-16 14:20:01 +0200

    Better naming for plotting transmitted pulse

--- e_fish/plot.py
+++ e_fish/plot.py
@@ -2,21 +2,43 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def plot_signals(df: pd.DataFrame, file_number:int):
+def plot_signals(df: pd.DataFrame, file_number: int):
+    df = df.query(f"file_number=={file_number}")
 
-    df=df.query(f"file_number=={file_number}")
-
-    plt.figure(figsize=(10,5))
-    plt.plot(df.time*1e9,df.incident,color="#D17E00", linestyle="-",linewidth=3,label="Incident")
-    plt.plot(df.time*1e9,df.reflected,color="#4A90E2",linestyle="-",linewidth=3,label="Reflected")
-    plt.hlines(0.05,xmin=df.time.min()*1e9,xmax=df.time.max()*1e9, color="k", linestyle="-.", label="Triggering value")
-    plt.plot(df.time*1e9,df.transmitted,color="g",linestyle="--", label="Deposited")
-    plt.legend(loc="best",fontsize=15)
-    plt.grid(True, which="both", linestyle="--", linewidth=0.5)
-    plt.xticks(fontsize=12)
-    plt.yticks(fontsize=12)
-    plt.xlabel("Time, ns", fontsize=15)
-    plt.ylabel("Voltage, a.u.", fontsize=15)
+    plt.figure(figsize=(10, 5))
+    plt.plot(
+        df.time * 1e9,
+        df.incident,
+        color="#D17E00",
+        linestyle="-",
+        linewidth=3,
+        label="Incident",
+    )
+    plt.plot(
+        df.time * 1e9,
+        df.reflected,
+        color="#4A90E2",
+        linestyle="-",
+        linewidth=3,
+        label="Reflected",
+    )
+    plt.hlines(
+        0.05,
+        xmin=df.time.min() * 1e9,
+        xmax=df.time.max() * 1e9,
+        color="k",
+        linestyle="-.",
+        label="Trigger value",
+    )
+    plt.plot(
+        df.time * 1e9, df.transmitted, color="g", linestyle="--", label="Deposited"
+    )
+    plt.legend(loc="best", fontsize=15)
+    # plt.grid(True, which="both", linestyle="--", linewidth=0.5)
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+    plt.xlabel("Time, ns", fontsize=20)
+    plt.ylabel("Voltage, a.u.", fontsize=20)
     plt.tight_layout()
-    plt.title("Caveat", fontsize=25)
+    plt.title("No deposited pulse", fontsize=25)
     return 0
